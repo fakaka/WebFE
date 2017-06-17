06f88b1c37e938c6568e259cc10cfeb9488fea52
hairpin <vsmj012@163.com>
2017-06-18 00:08:51 +0800

    remove mui recommend模块

---
+++ vuejs/vue-music/src/components/disc/disc.vue
@@ -0,0 +1,35 @@
+<template>
+    <transition name="slide">
+        <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
+    </transition>
+</template>
+
+<script>
+// import MusicList from 
+export default {
+    name: '',
+    props: {},
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+    },
+    computed: {
+    },
+    mount() {
+    },
+    components:{
+        MusicList
+    }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .slide-enter-active, .slide-leave-active
+    transition: all 0.3s
+
+  .slide-enter, .slide-leave-to
+    transform: translate3d(100%, 0, 0)
+</style>
--- vuejs/vue-music/src/router/index.js
+++ vuejs/vue-music/src/router/index.js
@@ -16,6 +16,7 @@ export default new Router({
             path: '/recommend',
             name: 'recommend',
             component: Recommend
-        }
+        },
+        
     ]
 })
