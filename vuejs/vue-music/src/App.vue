06c01437d6b3797fe30a69c9f2ca890505f2f0d8
hairpin <vsmj012@163.com>
2017-06-22 01:10:12 +0800

    播放器部分的vuex

--- vuejs/vue-music/src/App.vue
+++ vuejs/vue-music/src/App.vue
@@ -5,19 +5,18 @@
         <keep-alive>
             <router-view></router-view>
         </keep-alive>
-        <!--<player></player>-->
+        <player></player>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
 import MHeader from './components/m-header/m-header'
-// import Player from 'components/player/player'
+import Player from './components/player/player'
 import Tab from './components/tab/tab'
 
 export default {
     components: {
-        MHeader,
-        Tab
+        MHeader, Tab, Player
     }
 }
 </script>
--- vuejs/vue-music/src/components/music-list/music-list.vue
+++ vuejs/vue-music/src/components/music-list/music-list.vue
@@ -16,7 +16,7 @@
         <div class="bg-layer" ref="layer"></div>
         <scroll :data="songs" @scroll="scroll" :listen-scroll="listenScroll" :probe-type="probeType" class="list" ref="list">
             <div class="song-list-wrapper">
-                <song-list :songs="songs"></song-list>
+                <song-list :songs="songs" @select="selectItem"></song-list>
             </div>
             <div class="loading-container" v-show="!songs.length">
                 <loading></loading>
@@ -30,6 +30,7 @@ import Scroll from '../../base/scroll/scroll'
 import Loading from '../../base/loading/loading'
 import SongList from '../../base/song-list/song-list'
 import { prefixStyle } from '../../common/js/dom'
+import { mapActions } from 'vuex'
 
 const transform = prefixStyle('transform')
 const backdrop = prefixStyle('backdrop-filter')
@@ -64,7 +65,15 @@ export default {
         },
         random() {
             console.log('随机播放音乐')
-        }
+        },
+        selectItem(item, index) {
+            this.selectPlay({
+                list: this.songs, index
+            })
+        },
+        ...mapActions([
+            'selectPlay'
+        ])
     },
     watch: {
         // FIXME 结合mounted()函数,把代码修改的简洁一点
