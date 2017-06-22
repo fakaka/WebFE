c20c7b8a8296569792004a0d63012dbd228c9265
hairpin <vsmj012@163.com>
2017-06-22 11:53:05 +0800

    add 播放页面动画 fix getter.js的错误

--- vuejs/vue-music/src/components/player/player.vue
+++ vuejs/vue-music/src/components/player/player.vue
@@ -1,14 +1,60 @@
 <template>
     <div class="player" v-show="playList.length">
-        <div class="normal-player" v-show="fullScreen">
-            播放器页面
-        </div>
-        <div class="mini-player" v-show="!fullScreen"></div>
+        <transition name="normal" @enter="enter" @after-enter="afterEnter" @leave="leave" @after-leave="afterLeave">
+            <div class="normal-player" v-show="fullScreen">
+                <div class="background">
+                    <img width="100%" height="100%" src="" alt="">
+                </div>
+                <div class="top">
+                    <div class="back" @click="back">
+                        <i class="icon-back"></i>
+                    </div>
+                    <h1 class="title" v-html="currentSong.name"></h1>
+                    <h2 class="subtitle" v-html="currentSong.singer"></h2>
+                </div>
+                <div class="middle">
+                    <div class="middle-l">
+                        <div class="cd-wrapper" ref="cdWrapper">
+                            <div class="cd">
+                                <img :src="currentSong.image" class="image" alt="cover">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="operators">
+                        <div class="icon i-left">
+                            <i :class="iconMode"></i>
+                        </div>
+                        <div class="icon i-left" :class="disableCls">
+                            <i class="icon-prev"></i>
+                        </div>
+                        <div class="icon i-center" :class="disableCls">
+                            <i :class="playIcon"></i>
+                        </div>
+                        <div class="icon i-right" :class="disableCls">
+                            <i class="icon-next"></i>
+                        </div>
+                        <div class="icon i-right">
+                            <i class="icon"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </transition>
+        <!--<transition name="mini">
+                                                                                                <div class="mini-player" v-show="!fullScreen"></div>
+                                                                                            </transition>-->
     </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+import animations from 'create-keyframe-animation'
+import { prefixStyle } from 'common/js/dom'
+
+const transform = prefixStyle('transform')
+const transitionDuration = prefixStyle('transitionDuration')
 
 export default {
     name: 'player',
@@ -19,11 +65,78 @@ export default {
         }
     },
     methods: {
+        back() {
+            this.setFullScreen(false)
+        },
+        open() {
+            this.setFullScreen(true)
+        },
+        enter(el, done) {
+            const { x, y, scale } = this._getPosAndScale()
+
+            let animation = {
+                0: {
+                    transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+                },
+                60: {
+                    transform: `translate3d(0,0,0)scale(1.1)`
+                },
+                100: {
+                    transform: `translate3d(0,0,0)scale(1)`
+                }
+            }
+
+            animations.registerAnimation({
+                name: 'move',
+                animation,
+                presets: {
+                    duration: 400,
+                    easing: 'linear'
+                }
+            })
+            animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+        },
+        afterEnter() {
+            animations.unregisterAnimation('move')
+            this.$refs.cdWrapper.style.animation = ''
+        },
+        leave(el, done) {
+            this.$refs.cdWrapper.style.transition = 'all 0.4s'
+            const { x, y, scale } = this._getPosAndScale()
+            this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+            this.$refs.cdWrapper.addEventListener('transitionend', done)
+        },
+        afterLeave() {
+            this.$refs.cdWrapper.style.transition = ''
+            this.$refs.cdWrapper.style[transform] = ''
+        },
+
+        _getPosAndScale() {
+            const targetWidth = 40
+            const paddingLeft = 40
+            const paddingBottom = 30
+            const paddingTop = 80
+            const width = window.innerWidth * 0.8
+            const scale = targetWidth / width
+            const x = -(window.innerWidth / 2 - paddingLeft)
+            const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+            return {
+                x, y, scale
+            }
+        },
+        ...mapMutations({
+            setFullScreen: 'SET_FULL_SCREEN'
+        })
     },
     computed: {
+        playIcon() {
+            return this.playing ? 'icon-pause' : 'icon-play'
+        },
         ...mapGetters([
             'fullScreen',
-            'playList'
+            'playing',
+            'playList',
+            'currentSong'
         ])
     },
     mounted() {
