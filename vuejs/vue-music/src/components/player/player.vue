08a4bb2a0b0a57d2f1ebce1d824521e8b599f1b6
hairpin <vsmj012@163.com>
2017-06-22 13:46:27 +0800

    播放音乐，基本的切换歌曲

--- vuejs/vue-music/src/components/player/player.vue
+++ vuejs/vue-music/src/components/player/player.vue
@@ -13,9 +13,9 @@
                     <h2 class="subtitle" v-html="currentSong.singer"></h2>
                 </div>
                 <div class="middle">
-                    <div class="middle-l">
+                    <div class="middle-l" ref="middleL">
                         <div class="cd-wrapper" ref="cdWrapper">
-                            <div class="cd">
+                            <div class="cd" :class="cdCls">
                                 <img :src="currentSong.image" class="image" alt="cover">
                             </div>
                         </div>
@@ -24,27 +24,44 @@
                 <div class="bottom">
                     <div class="operators">
                         <div class="icon i-left">
-                            <i :class="iconMode"></i>
+                            <i class="icon-sequence"></i>
                         </div>
                         <div class="icon i-left" :class="disableCls">
-                            <i class="icon-prev"></i>
+                            <i class="icon-prev" @click="prev"></i>
                         </div>
                         <div class="icon i-center" :class="disableCls">
-                            <i :class="playIcon"></i>
+                            <i :class="playIcon" @click="togglePlaying"></i>
                         </div>
                         <div class="icon i-right" :class="disableCls">
-                            <i class="icon-next"></i>
+                            <i class="icon-next" @click="next"></i>
                         </div>
                         <div class="icon i-right">
-                            <i class="icon"></i>
+                            <i class="icon icon-favorite"></i>
                         </div>
                     </div>
                 </div>
             </div>
         </transition>
-        <!--<transition name="mini">
-                                                                                                <div class="mini-player" v-show="!fullScreen"></div>
-                                                                                            </transition>-->
+        <transition name="mini">
+            <div class="mini-player" v-show="!fullScreen">
+                <div class="icon">
+                    <img :class="cdCls" width="40" height="40" :src="currentSong.image">
+                </div>
+                <div class="text">
+                    <h2 class="name" v-html="currentSong.name"></h2>
+                    <p class="desc" v-html="currentSong.singer"></p>
+                </div>
+                <div class="control">
+                    <div class="progress-circle">
+                        <i @click.stop="togglePlaying" :class="miniIcon"></i>
+                    </div>
+                </div>
+                <div class="control" @click.stop="showPlaylist">
+                    <i class="icon-playlist"></i>
+                </div>
+            </div>
+        </transition>
+        <audio :src="currentSong.url" @play="ready" @error="error" @timeupdate="" ref="audio"></audio>
     </div>
 </template>
 
@@ -61,7 +78,7 @@ export default {
     props: {},
     data() {
         return {
-
+            songReady: false
         }
     },
     methods: {
@@ -69,6 +86,48 @@ export default {
             this.setFullScreen(false)
         },
         open() {
+            this.setFullScreen(true)
+        },
+        next() {
+            console.log('songReady = ' + this.songReady)
+            if (!this.songReady) {
+                return
+            }
+            console.log('next')
+            let index = this.currentIndex + 1
+            if (index === this.playList.length) {
+                index = 0
+            }
+            this.setCurrentIndex(index)
+            this.songReady = false
+        },
+        prev() {
+            if (!this.songReady) {
+                return
+            }
+            let index = this.currentIndex - 1
+            if (index === -1) {
+                index = 0
+            }
+            this.setCurrentIndex(index)
+            this.songReady = false
+        },
+        ready() {
+            this.songReady = true
+        },
+        error() {
+            this.songReady = false
+        },
+        togglePlaying() {
+            if (!this.songReady) {
+                return
+            }
+            this.setPlayingState(!this.playing)
+            // if (this.currentLyric) {
+            //     this.currentLyric.togglePlay()
+            // }
+        },
+        showPlaylist() {
             this.setFullScreen(true)
         },
         enter(el, done) {
@@ -110,7 +169,6 @@ export default {
             this.$refs.cdWrapper.style.transition = ''
             this.$refs.cdWrapper.style[transform] = ''
         },
-
         _getPosAndScale() {
             const targetWidth = 40
             const paddingLeft = 40
@@ -125,21 +183,46 @@ export default {
             }
         },
         ...mapMutations({
-            setFullScreen: 'SET_FULL_SCREEN'
+            setFullScreen: 'SET_FULL_SCREEN',
+            setPlayingState: 'SET_PLAYING_STATE',
+            setCurrentIndex: 'SET_CURRENT_INDEX'
         })
     },
     computed: {
         playIcon() {
             return this.playing ? 'icon-pause' : 'icon-play'
         },
+        miniIcon() {
+            return this.playing ? 'icon-pause-mini' : 'icon-play-mini'
+        },
+        cdCls() {
+            return this.playing ? 'play' : 'play pause'
+        },
+        disableCls() {
+            return this.songReady ? '' : 'disable'
+        },
         ...mapGetters([
             'fullScreen',
             'playing',
             'playList',
-            'currentSong'
+            'currentSong',
+            'currentIndex'
         ])
     },
     mounted() {
+    },
+    watch: {
+        currentSong() {
+            this.$nextTick(() => {
+                this.$refs.audio.play()
+            })
+        },
+        playing(newPlaying) {
+            const audio = this.$refs.audio
+            this.$nextTick(() => {
+                newPlaying ? audio.play() : audio.pause()
+            })
+        }
     }
 }
 </script>
