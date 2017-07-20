91c651239f4dd45f131fb62723e36e35dbce11fc
hairpin <vsmj012@163.com>
2017-07-20 20:33:06 +0800

    vue格式的相关配置

--- vuejs/MUI/src/toast/toast.vue
+++ vuejs/MUI/src/toast/toast.vue
@@ -1,11 +1,9 @@
 <template>
-        <div class="mu-toast">
-            {{message}}
-        </div>
+    <div class="mu-toast">
+        {{message}}
+    </div>
 </template>
-
 <script>
-
 export default {
     name: 'mu-toast',
     props: {
@@ -25,19 +23,18 @@ export default {
     }
 }
 </script>
-
 <style lang="less">
-.mu-toast {
-    height: 48px;
-    line-height: 48px;
-    padding: 0 24px;
-    background-color: #fff;
-    color: #000;
-    border-radius: 24px;
-    max-width: 568px;
-    width: 100%;
-    position: fixed;
-    left: 0;
-    bottom: 0;
-}
+    .mu-toast {
+        height: 48px;
+        line-height: 48px;
+        padding: 0 24px;
+        background-color: #fff;
+        color: #000;
+        border-radius: 24px;
+        max-width: 568px;
+        width: 100%;
+        position: fixed;
+        left: 0;
+        bottom: 0;
+    }
 </style>
